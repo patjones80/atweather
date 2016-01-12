9d8170c19f11c8518cb6a879c10f01623122103f
Patrick Jones <patjones80@gmail.com>
2016-01-12 18:32:34 -0500

    Server side improvements (views.py, index.html, 404 error handling)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 from app import views
+
+@app.errorhandler(404)
+
+def page_not_found(e):
+    
+    return render_template("404.html")
--- app/views.py
+++ app/views.py
@@ -1,85 +1,137 @@
 
-# laptop dev path:  c:/Python34/atweather
+"""
+views.py
 
-from flask import render_template, request
-from app import app
+Implements the views module necessary for development with
+the Flask web framework.
 
+"""
+
+import os
 import requests
+
+from flask import render_template, request, Markup
+from app   import app
+
+# define the parent directory for the project so that we don't need absolute file paths
+
+PARENT_DIR = os.path.join(os.path.dirname(__file__), '..')
+
+# pull the full location list just once
+
+LOCATIONS = []
+
+with open('%s/at_shelter_list.txt' % PARENT_DIR, 'r') as LocationFile:
+
+    for line in LocationFile:
+        cols = line.split('\t')
+
+        # location id, location name, NOAA URL, state abbreviation, placeholder for selected tag
+        LOCATIONS.append([int(cols[0]), cols[1], cols[5].rstrip(), cols[6].rstrip('\n'), ''])
+
+print(__doc__)
 
 def get_html(url):
 
+    """
+
+    The full NOAA weather forecast URL for each location is stored in
+    LOCATIONS, which in turn is sourced from at_shelter_list.txt (see above).
+
+    We submit this URL to NOAA and pull the HTML for their text-only
+    weather forecast, which we use for display of the forecast on atweather.
+
+    If something goes wrong, NOAA returns a '404 Not Found', which we check
+    for. If there are any other problems, we check for a general exception. In
+    either case, we return False so that an error can be displayed.
+
+    """
+
+    exception_thrown = False
+
     # return the HTML that sources the URL passed into the function
-    # the proxies argument is required for the production version of the code on PythonAnywhere's server
+    # the proxies argument is required for PythonAnywhere's server
 
-    page = requests.get(url) #, proxies = {'http':'http://proxy.server:3128'})
-    html_string = page.text
+    try:
 
-    return html_string
+        page = requests.get(url) #, proxies = {'http':'http://proxy.server:3128'})
+        html_string = page.text
 
-def write_nws_html(url):
+    except requests.exceptions.RequestException:
 
-    # write the HTML for the text forecast to nws.html, which will "extend" the base HTML file index.html
-    html_string = get_html(url)
+        exception_thrown = True
 
-    # minor formatting adjustment(s)
-    html_string = html_string.replace('<b>Last Update: </b></a>','<b>Last Update</b></a>: ')
-    html_string = html_string.replace('margin-left:-40px;','')
-    html_string = html_string.replace('font-family: Arial !important', 'font-family: \'Trebuchet MS\', Helvetica, sans-serif')
-    html_string = html_string.replace('<br>&nbsp;', ', ')
+    # formatting changes to what we are bringing in from NOAA (listed in the text file shown)
 
-    # the watch/warning/hazardous weather links all point to a PHP script on the NWS page; these links don't work from an external HTML source unless appended like this:
-    html_string = html_string.replace('showsigwx.php','http://forecast.weather.gov/showsigwx.php')
+    with open('%s/formatting_adjustments.txt' % PARENT_DIR, 'r') as format_changes:
+        for line in format_changes:
+            html_string = html_string.replace(line.split('\t')[0], line.split('\t')[1])
 
-    f = open('c:/Python34/atweather/app/templates/nws.html', 'w')
+    # the links to hazardous weather warnings embedded in the NOAA page don't work unless
+    # replaced with the absolute URL
 
-    f.write('{% extends "index.html" %}\n{% block forecast %}\n' + html_string + '\n{% endblock %}')
-    f.close()
+    html_string = html_string.replace('showsigwx.php', 'http://forecast.weather.gov/showsigwx.php')
 
-def init_shelter_list():
-
-    # pull the shelters and URLs into a list and return it for initialization
-
-    L = []
-
-    f = open('c:/Python34/atweather/at_shelter_list.txt', 'r')
-
-    for line in f:
-        L.append([int(line.split('\t')[0]), line.split('\t')[1], line.split('\t')[2], line.split('\t')[5].rstrip(), line.split('\t')[6], ''])
-
-    f.close()
-
-    return L
-
-# home page
+    if html_string.find('404 Not Found') >= 0 or exception_thrown:
+        return False
+    else:
+        return html_string
 
 @app.route('/')
+
 def index():
 
-    return render_template('index.html', shelter_list = init_shelter_list(), shelter_list_full = init_shelter_list())
+    """
+    Handles rendering of the home page
 
-# probability of precipitation (pop) discussion
+    """
+
+    return render_template('index.html', shelter_list = LOCATIONS,
+                           shelter_list_full = LOCATIONS)
 
 @app.route('/precip_discussion')
+
 def pop_discussion():
 
-    return render_template('pop_discussion.html', shelter_list = init_shelter_list(), shelter_list_full = init_shelter_list())
+    """
+    Handles rendering of the probability of precipitation discussion
 
-# something from the shelter list was selected
+    """
 
-@app.route('/forecast/loc_id=<int:loc_id>', methods = ['GET','POST'])
+    return render_template('pop_discussion.html', shelter_list = LOCATIONS,
+                           shelter_list_full = LOCATIONS)
+
+@app.route('/forecast', methods = ['GET'])
+
 def forecast(loc_id = None):
 
-    L = init_shelter_list()
-    
-    if request.method == 'GET':
-        
-        if loc_id:
-            url = L[loc_id-1][3]
-            write_nws_html(url)
-            
-            for s in L:
-                if s[0] == loc_id:
-                    s[3] = 'selected'
+    """
+    Handles rendering of page after a location has been selected
 
-            return render_template('nws.html', shelter_list = [s for s in L if s[4] == L[loc_id-1][4]], selected_state = L[loc_id-1][4], shelter_list_full = init_shelter_list())
+    """
+
+    loc_state = request.args['myState']
+    loc_id = int(request.args['myShelter'])
+
+    shelter_list = LOCATIONS
+
+    if loc_id:
+
+        # label this element in the list as selected so that it stays selected on re-render
+        shelter_list[loc_id - 1][4] = 'selected'
+
+        # URL to submit to NOAA
+        url = shelter_list[loc_id - 1][2]
+
+        # if the user filtered the state list, make sure it stays filtered on re-render
+        if len(loc_state) > 0:
+            shelter_list = [s for s in shelter_list if s[3] == loc_state]
+
+        if get_html(url) is False:
+            return render_template('404_NOAA.html', shelter_list = shelter_list,
+                                   shelter_list_full = LOCATIONS)
+        else:
+            return render_template('index.html', shelter_list = shelter_list,
+                                   shelter_list_full = LOCATIONS,
+                                   forecast_text = Markup(get_html(url)))
 
