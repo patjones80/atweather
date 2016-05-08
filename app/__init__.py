1aa4dcefe96c2ba555e34a1b62deaf3d13f0f728
patjones80 <patjones80@gmail.com>
2016-05-08 09:25:16 -0700

    Addition of PCT to trail list; forced HTTPS implemented

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 
 from flask import Flask, render_template
 
+#from flask_sslify import SSLify
+
 app = Flask(__name__)
+
+#sslify = SSLify(app)
 
 from app import views
 
-@app.errorhandler(404)
+##@app.errorhandler(404)
+##
+##def page_not_found(e):
+##    
+##    return render_template("404.html")
 
-def page_not_found(e):
-    
-    return render_template("404.html")
--- app/views.py
+++ app/views.py
@@ -13,23 +13,61 @@ import requests
 from flask import render_template, request, Markup
 from app   import app
 
-# define the parent directory for the project so that we don't need absolute file paths
+def get_locations():
 
-PARENT_DIR = os.path.join(os.path.dirname(__file__), '..')
+    LOCATIONS = []
+    
+    with open('%s/at_shelter_list.txt' % PARENT_DIR, mode = 'r', encoding = 'UTF-8') as LocationFile:
 
-# pull the full location list just once
+        for line in LocationFile:
+            cols = line.split('\t')
 
-LOCATIONS = []
+            # location id, location name, NOAA URL, state abbreviation, placeholder for selected tag
+            LOCATIONS.append([int(cols[0]), cols[1], cols[5].rstrip(), cols[6].rstrip('\n'), ''])
 
-with open('%s/at_shelter_list.txt' % PARENT_DIR, 'r') as LocationFile:
+    return LOCATIONS
 
-    for line in LocationFile:
-        cols = line.split('\t')
+def get_states(trail_selection = None, state_selection = None):
 
-        # location id, location name, NOAA URL, state abbreviation, placeholder for selected tag
-        LOCATIONS.append([int(cols[0]), cols[1], cols[5].rstrip(), cols[6].rstrip('\n'), ''])
+    states_at = [['GA','AT - Georgia','','AT'],
+                 ['NC','AT - North Carolina','','AT'],
+                 ['TN','AT - Tennessee','','AT'],
+                 ['VA','AT - Virginia','','AT'],
+                 ['MD','AT - Maryland','','AT'],
+                 ['PA','AT - Pennsylvania','','AT'],
+                 ['NJ','AT - New Jersey','','AT'],
+                 ['NY','AT - New York','','AT'],
+                 ['CT','AT - Connecticut','','AT'],
+                 ['MA','AT - Massachusetts','','AT'],
+                 ['VT','AT - Vermont','','AT'],
+                 ['NH','AT - New Hampshire','','AT'],
+                 ['ME','AT - Maine','','AT']]
 
-print(__doc__)
+    states_pct = [['CA','PCT - California','','PCT'],
+                  ['OR','PCT - Oregon','','PCT'],
+                  ['WA','PCT - Washington','','PCT']]
+
+    if trail_selection == None:
+        states = states_at + states_pct
+    elif trail_selection == 'AT':
+        states = states_at
+    else:
+        states = states_pct
+    
+    for s in states:
+        if s[0] == state_selection:
+            s[2] = 'selected'
+        
+    return states
+
+def get_trails():
+
+    # define the trail list
+
+    TRAILS = []
+    TRAILS = [['AT','Appalachian Trail',''],['PCT','Pacific Crest Trail','']]
+
+    return TRAILS
 
 def get_html(url):
 
@@ -63,7 +101,7 @@ def get_html(url):
 
     # formatting changes to what we are bringing in from NOAA (listed in the text file shown)
 
-    with open('%s/formatting_adjustments.txt' % PARENT_DIR, 'r') as format_changes:
+    with open('%s/formatting_adjustments.txt' % PARENT_DIR, mode = 'r', encoding = 'ISO-8859-1') as format_changes:
         for line in format_changes:
             html_string = html_string.replace(line.split('\t')[0], line.split('\t')[1])
 
@@ -85,9 +123,8 @@ def index():
     Handles rendering of the home page
 
     """
-
-    return render_template('index.html', shelter_list = LOCATIONS,
-                           shelter_list_full = LOCATIONS)
+    
+    return render_template('index.html', **context_default)
 
 @app.route('/precip_discussion')
 
@@ -98,40 +135,80 @@ def pop_discussion():
 
     """
 
-    return render_template('pop_discussion.html', shelter_list = LOCATIONS,
-                           shelter_list_full = LOCATIONS)
+    return render_template('pop_discussion.html', **context_default)
 
 @app.route('/forecast', methods = ['GET'])
 
-def forecast(loc_id = None):
+def forecast():
 
     """
     Handles rendering of page after a location has been selected
 
     """
 
-    loc_state = request.args['myState']
-    loc_id = int(request.args['myShelter'])
+    (loc_state, loc_id) = (request.args['myState'], int(request.args['myShelter']))    
+    (trail_list, shelter_list) = (get_trails(), get_locations())
 
-    shelter_list = LOCATIONS
-
+    loc_trail = request.args.get('myTrail','AT')
+    
     if loc_id:
 
-        # label this element in the list as selected so that it stays selected on re-render
+        # retain our trail selection on re-render        
+        for trail in trail_list:
+            if trail[0] == loc_trail:
+                trail[2] = 'selected'
+            else:
+                trail[2] = ''
+        
+        # retain our shelter/location selection on re-render        
         shelter_list[loc_id - 1][4] = 'selected'
 
-        # URL to submit to NOAA
+        # URL to submit to NOAA        
         url = shelter_list[loc_id - 1][2]
 
-        # if the user filtered the state list, make sure it stays filtered on re-render
-        if len(loc_state) > 0:
+        # if the user filtered the shelter list, make sure it stays filtered on re-render
+        state_list = get_states(loc_trail, loc_state)
+        state_abbr = tuple(M[0] for M in state_list)
+            
+        if loc_state:
+            # user filtered by state
             shelter_list = [s for s in shelter_list if s[3] == loc_state]
+        elif loc_trail:
+            # user only filtered by trail
+            shelter_list = [s for s in shelter_list if s[3] in state_abbr]            
 
-        if get_html(url) is False:
-            return render_template('404_NOAA.html', shelter_list = shelter_list,
-                                   shelter_list_full = LOCATIONS)
-        else:
-            return render_template('index.html', shelter_list = shelter_list,
-                                   shelter_list_full = LOCATIONS,
-                                   forecast_text = Markup(get_html(url)))
+        context = { 'trail_list': trail_list,
+                    'state_list': state_list,
+                    'state_list_full': get_states(),
+                    'shelter_list': shelter_list[:],
+                    'shelter_list_full': LOCATIONS }
+        
+        if get_html(url) is False:        
+            return render_template('404_NOAA.html', **context)
+        
+        else:            
+            return render_template('index.html', forecast_text = Markup(get_html(url)), **context)
 
+@app.errorhandler(404)
+
+def page_not_found(e):
+    
+    return render_template('404.html', **context_default)
+
+# define the parent directory for the project so that we don't need absolute file paths
+
+PARENT_DIR = os.path.join(os.path.dirname(__file__), '..')
+
+# pull the full location list just once
+
+LOCATIONS = get_locations()
+
+# construct the default list of template arguments
+
+context_default = { 'trail_list': get_trails(),
+                    'state_list': get_states(),
+                    'state_list_full': get_states(),
+                    'shelter_list': LOCATIONS,
+                    'shelter_list_full': LOCATIONS }
+
+print(__doc__)
