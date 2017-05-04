4c952e3e86d561d6a2d7eccd5cd6bc579c025094
patjones80 <patjones80@gmail.com>
2017-05-04 00:07:03 -0700

    Stable API version; disclaimer added

--- app/funcLib.py
+++ app/funcLib.py
@@ -6,141 +6,65 @@ Functions available to views.py for obtaining the weather forecast as a function
 
 '''
 
-import os
-import requests
+import os, requests, collections
+
+# define named tuple structure that will make up the locations dictionary
+Location = collections.namedtuple('Location', 'Name Longitude Latitude State')
 
 # these are the column numbers in the source file
-
 (LOC_ID, LOC, LON, LAT, STATE) = (0, 1, 2, 3, 4)
 
 # project's root directory
-
-PARENT_DIR = os.path.join(os.path.dirname(__file__), '..')
+PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 
 # define the National Weather Service URL for pulling forecasts
 
-# will be deprecated!
-NWS_URL = 'http://forecast.weather.gov/MapClick.php?lon=%s&lat=%s&unit=0&lg=english&FcstType=text&TextType=1'
-
-# TODO: change the development API endpoint to the production endpoint
-# NWS_URL = 'https://api-v1.weather.gov/points/%s,%s/forecast'
-
-#####
-
-class Location():
-
-    def __init__(self, curr_id):
-
-        ForecastPoints = getLocationList()
-
-        self.Name      = ForecastPoints[curr_id][LOC]
-        self.Longitude = ForecastPoints[curr_id][LON]
-        self.Latitude  = ForecastPoints[curr_id][LAT]
-        self.State     = ForecastPoints[curr_id][STATE]
-
-#####
+NWS_URL = 'https://api.weather.gov/points/%s,%s/forecast'
         
 def getLocationList():
+
+	LOCATIONS = dict()
+
+	# use forward slashes in unix/linux
     
-    LOCATIONS = dict()
+	if os.name == 'posix':
+		f_locations = r'%s/at_shelter_list.txt' % PARENT_DIR
+	else:
+		f_locations = r'%s\at_shelter_list.txt' % PARENT_DIR
 
-    with open('%s/at_shelter_list.txt' % PARENT_DIR, mode = 'r', encoding = 'UTF-8') as LocationFile:
+	# proceed with reading the location list in
 
-        for line in LocationFile:
+	with open(f_locations, mode = 'r', encoding = 'UTF-8') as LocationFile:
 
-            line = line.strip('\n')
-            
-            cols = list(line.split('\t'))
-            
-            cols[0] = int(cols[0])
-            
-            LOCATIONS[int(cols[LOC_ID])] = cols            
-            
-    return LOCATIONS
+		for line in LocationFile:
+			line = list(line.strip('\n').split('\t'))
+			cols = Location(Name = line[LOC], Longitude = line[LON], Latitude = line[LAT], State = line[STATE])
 
-#####
-
-def AdjustHTML(html):
-
-    # makes several formatting changes to the HTML returned from the NWS website
-    
-    formatted = html
-    formatted = formatted.replace('<b>Last Update: </b></a>', '<b>Last Update</b></a>:')                                    # bring the colon outside the link text
-    formatted = formatted.replace('margin-left:-40px;', '')                                                                 # dump the NWS's margin formatting
-    formatted = formatted.replace('font-family: Arial !important', 'font-family: Trebuchet MS, Helvetica, sans-serif')      # modify the NWS's default font
-    formatted = formatted.replace('<br>&nbsp;', ',')                                                                        # dump the line break
-
-    # the links to hazardous weather warnings embedded in the NOAA page don't work unless replaced with the absolute URL
-
-    formatted = formatted.replace('showsigwx.php', 'http://forecast.weather.gov/showsigwx.php')
-
-    return formatted
+			LOCATIONS[int(line[LOC_ID])] = cols
+	
+	return LOCATIONS
 
 #####
 
 def getForecast(lat, lon):
-
-    forecast_html = ''
     
     try:
 
-        # for checking our logic
-        # raise Exception('This is a bug, dude...', 123)
-
-        # for production
-        # forecast_html = requests.get(NWS_URL % (lon, lat), proxies = {'http':'http://proxy.server:3128'}).text
-
-        # will be deprecated!
-        
-        forecast_html = requests.get(NWS_URL % (lon, lat)).text
-
-
-        # NWS API formalism, now on hold until March 2017
+        # NWS API formalism
         
         # TODO: insert proper header(s)
         # TODO: need to pass along proper SSL certification!
-        # TODO: insert proxies argument for production?
-
-##        print(NWS_URL % (lat, lon))
         
-##        response = requests.get(NWS_URL % (lat, lon), headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36" }, verify = False)
-
-##        print('The response is: ')
-##        print(response)
-##        
-##        forecast = response.json()
+        response = requests.get(NWS_URL % (lat, lon), headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36" }, verify = False)
+        forecast = response.json()
         
     except Exception as e:
 
         # check first for general exceptions
         return 'There was a error retrieving the forecast from the National Weather Service: %s' % repr(e)      
 
-    finally:
-
-        # TODO: deprecate this error branch?
-        
-        if forecast_html.find('404 Not Found') >= 0:                                                            
-
-            # then check if the HTML returned contained a 404 message
-
-            return 'The National Weather Service returned a 404 error.'
-
-        pass
     
     # otherwise, we're good to go
     
-    return AdjustHTML(forecast_html)
-
-##    return forecast
+    return forecast
     
-#####    
-
-# testing
-
-# curr_id = 0
-# curr_id = int(input("Enter location ID: "))
-
-# current_location = Location(curr_id = curr_id)
-
-# print(getForecast(current_location.Latitude, current_location.Longitude))
-
--- app/views.py
+++ app/views.py
@@ -25,11 +25,11 @@ def index():
     return render_template('index.html', **context_default)
 
 
-@app.route('/changes_discussion')
+@app.route('/disclaimer')
 
 def changes_discussion():
 
-    return render_template('changes_discussion.html', **context_default)
+    return render_template('disclaimer.html', **context_default)
 
 @app.route('/precip_discussion')
 
@@ -62,76 +62,67 @@ def forecast():
 
     """
 
-    (loc_trail, loc_state, loc_id) = (request.args['myTrail'], request.args['myState'], int(request.args['myShelter'])) 
-        
-    # retain trail selection
+    (loc_trail, loc_state, loc_id) = (request.args['myTrail'], request.args['myState'], int(request.args['myShelter']))  
     
-    trail_list = dropdown_items.trails[:]   
-
-#    mark_selected(trail_list, loc_trail, 2)
-    
-    # retain selection of state or park name
+    # keep the state list filtered after forecast selection
     
     if loc_trail   == 'AT':        
-        state_list = dropdown_items.states_at[:]
+        state_list = dropdown_items.states_at
         
     elif loc_trail == 'PCT':
-        state_list = dropdown_items.states_pct[:]
+        state_list = dropdown_items.states_pct
 
     elif loc_trail == 'NP':
-        state_list = dropdown_items.states_np[:]
+        state_list = dropdown_items.states_np
         
     else:
-        state_list = dropdown_items.all_states[:]
-    
-#    mark_selected(state_list, loc_state, 2)
+        state_list = dropdown_items.all_states
 
     # keep location list filtered 
     
-    loc_list = list(LOCATIONS.values())[:]
-        
+    loc_list = [(j, LOCATIONS[j].Name, LOCATIONS[j].State) for j in LOCATIONS.keys()]
     state_abbr = tuple(M[0] for M in state_list)
             
     if loc_state:
         
         # user filtered by state
-        loc_list = [s for s in loc_list if s[4] == loc_state]
+        loc_list = [t for t in loc_list if t[2] == loc_state]
         
     elif loc_trail:
         
         # user only filtered by trail
-        loc_list = [s for s in loc_list if s[4] in state_abbr]
+        loc_list = [t for t in loc_list if t[2] in state_abbr]
     
     # save location
-
+    
     for item in loc_list:
-
-        print(item)
         
         if item[0] == loc_id:
-            location_to_display = 'Selected location: ' + item[1]
+            location_to_display = item[1]
 
-    context = { 'trail_list':      trail_list,
-                'state_list':      state_list,
-                'state_list_full': dropdown_items.all_states,
-                'shelter_list':      loc_list,
-                'shelter_list_full': list(LOCATIONS.values()),
+    context = { 'trail_list'       : dropdown_items.trails,
+                'state_list'       : state_list,
+                'state_list_full'  : dropdown_items.all_states,
+                'shelter_list'     : loc_list,
+                'shelter_list_full': T,
                 'location_selected': location_to_display}
 
-    fcstLocation = funcLib.Location(curr_id = loc_id)
-    
-    return render_template('index.html', forecast_text = Markup(funcLib.getForecast(fcstLocation.Latitude, fcstLocation.Longitude)), **context)    
+    # the requested location
+    fcstLocation = LOCATIONS[loc_id]
 
+    # the actual forecast, returned as a JSON dictionary
+    forecast = funcLib.getForecast(fcstLocation.Latitude, fcstLocation.Longitude)
 
-    # this is in preparation for the arrival of the new NWS API; see NWS API reference for structure of returned JSON
-    
-##    forecast = funcLib.getForecast(fcstLocation.Latitude, fcstLocation.Longitude) #['properties']['periods']
-##
-##    if isinstance(forecast, str):
-##        return render_template('index.html', err_msg = forecast, **context)
-##    else:
-##        return render_template('index.html', forecast = forecast['properties']['periods'], location_to_display = location_to_display, **context)
+    if isinstance(forecast, dict):
 
+        try:
+            return render_template('index.html', forecast = forecast['properties']['periods'], location_to_display = location_to_display, **context)        
+        except KeyError:
+            return render_template('index.html', err_msg = 'The forecast data for this location has expired. Please check again later for an updated forecast.', **context)
+            
+    else:
+        
+        return render_template('index.html', err_msg = forecast, **context)
 
 @app.errorhandler(404)
 
@@ -139,26 +130,6 @@ def page_not_found(e):
     
     return render_template('404.html', **context_default)
 
-
-def mark_selected(L, selected_item, select_column):
-
-    '''
-    Deprecated as of December 2016 update.
-
-    At different points, we want a selection from a dropdown list to remain selected when the template gets rendered. Since
-    each call of render_template does this, we need to have a function that we can pass a dropdown list into with the item
-    that the user selected, and ensure that the 'selected' marker gets attached to the dropdown list in question when we pass
-    it back to the client for rendering.
-
-    This is cumbersome, but the best I can come up with right now.
-
-    '''
-
-    for item in L:
-        if item[0] == selected_item:
-            item[select_column] = 'selected'
-        else:
-            item[select_column] = ''
             
 # pull the full location list just once
 
@@ -166,8 +137,10 @@ LOCATIONS = funcLib.getLocationList()
 
 # construct the default list of template arguments
 
-context_default = { 'trail_list':      dropdown_items.trails[:],
-                    'state_list':      dropdown_items.all_states[:],
+T = [(j, LOCATIONS[j].Name, LOCATIONS[j].State) for j in LOCATIONS.keys()]
+
+context_default = { 'trail_list':      dropdown_items.trails,
+                    'state_list':      dropdown_items.all_states,
                     'state_list_full': dropdown_items.all_states,
-                    'shelter_list':      list(LOCATIONS.values())[:],
-                    'shelter_list_full': list(LOCATIONS.values()) }            
+                    'shelter_list':      T,
+                    'shelter_list_full': T }            
