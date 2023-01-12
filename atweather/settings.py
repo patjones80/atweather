77c264813baf0a0c931bdc79955e1483837d06a1
unknown <patjones80@gmail.com>
2023-01-11 22:35:34 -0800

    Add prod version of settings

--- atweather/settings.py
+++ atweather/settings.py
@@ -18,13 +18,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'piekr$g_2i08+v&ey*7xc$ovbtr1cw%hm5unda#1fbf!91#z7)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.206', '127.0.0.1']
+ALLOWED_HOSTS = ['www.atweather.org']
 
 # Application definition
 
@@ -53,7 +50,7 @@ ROOT_URLCONF = 'atweather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['atwx1/templates/atwx1/'],
+        'DIRS': ['atwx1/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,5 +112,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-	
+
+STATIC_ROOT = '/home/pjones/atweather/atwx1/static'	
 STATIC_URL  = '/static/'
